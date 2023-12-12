6564017411f7f7df3320c1c6647706ebfaea1c6a
Moon Knight <103847581+Twilight-Killer@users.noreply.github.com>
2023-12-12 09:40:47 +0700

    Update __init__.py
    Signed-off-by: Moon Knight <103847581+Twilight-Killer@users.noreply.github.com>

--- FallenMusic/__init__.py
+++ FallenMusic/__init__.py
@@ -92,8 +92,8 @@ async def fallen_startup():
     ASS_USERNAME = getme2.username
     ASS_MENTION = getme2.mention
     try:
-        await app2.join_chat("DevilsHeavenMF")
-        await app2.join_chat("FallenAssociation")
+        await app2.join_chat("HaoTogelLivedraw")
+        await app2.join_chat("haotogel_result")
     except:
         pass
 
